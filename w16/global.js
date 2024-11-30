a098dae134d8fd5f3d14bee327130e727ed0ac06
JLiscano1 <joeistheboss001@gmail.com>
2024-11-29 20:33:12 -0800

    w16 setup done

---
+++ w16/global.js
@@ -0,0 +1,9 @@
+const TBL = document.getElementById("tab-data");
+const FORM = document.getElementById("form");
+const FNAME = document.getElementById("firstName");
+const LNAME = document.getElementById("lastName");
+const SUBMIT = document.getElementById("submitError");
+const WATER = document.getElementById("water");
+const DISHWASH = document.getElementById("dishWash");
+
+export {TBL, FORM, FNAME, LNAME, SUBMIT, WATER, DISHWASH};
